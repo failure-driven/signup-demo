9ea71d5959d4c2180d972b0d4218a441fc4a56ea
Michael Milewski <saramic@gmail.com>
2019-07-16 04:52:17 +1000

    User.username not valid with SPACE test
    failing
    User
      username
        not valid characters
          is not valid with SPACE (FAILED - 1)
    
    Failures:
    
      1) User username not valid characters is not valid with SPACE
         Failure/Error: expect(user).to_not be_valid
           expected #<User id: nil, name: "", username: "with space", created_at: nil, updated_at: nil> not to be valid
         # ./spec/models/user_spec.rb:13:in `block (4 levels) in <top (required)>'

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,13 @@ RSpec.describe User, type: :model do
       user = User.new(username: 'abc123')
       expect(user).to be_valid
     end
+
+    context 'not valid characters' do
+      it 'is not valid with SPACE' do
+        user = User.new(username: 'with space')
+        expect(user).to_not be_valid
+        expect(user.errors.messages).to eq(username: ["cannot have white space"])
+      end
+    end
   end
 end
