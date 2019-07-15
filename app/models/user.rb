f39a7512b4037a60d393bf325425979b6bdcfeea
Michael Milewski <saramic@gmail.com>
2019-07-16 05:22:51 +1000

    User.username not valid with ? implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,9 @@ class User < ApplicationRecord
      if username.index(' ') or username.index("\t")
        errors.add(:username, 'cannot have white space')
      end
+
+     if username.index('?')
+       errors.add(:username, 'cannot have question mark')
+     end
    end
 end
