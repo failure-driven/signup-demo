75f7bd59906fc38c27aec0a4965d66827dd9e569
Michael Milewski <saramic@gmail.com>
2019-07-16 04:49:43 +1000

    a first test for User.username happy path
    a username can be valid alpha numeric characters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'username' do
+    it 'is valid for alpha numeric characters' do
+      user = User.new(username: 'abc123')
+      expect(user).to be_valid
+    end
+  end
 end
