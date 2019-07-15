158bfa716336747403e7e5d8eb08ca5a856f53b1
Michael Milewski <saramic@gmail.com>
2019-07-16 04:55:04 +1000

    User.username not valid with SPACE implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+   validate :validate_username
+
+   private
+
+   def validate_username
+     if username.index(' ')
+       errors.add(:username, 'cannot have white space')
+     end
+   end
 end
