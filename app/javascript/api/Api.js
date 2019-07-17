3c7361956079e1fbae0a80bda0d79d3dea6870d5
Michael Milewski <saramic@gmail.com>
2019-07-17 23:28:28 +1000

    WIP use attempt endpoint to check for valid username

---
+++ app/javascript/api/Api.js
@@ -0,0 +1,21 @@
+const attempt = username => {
+  return fetch(`/api/attempt/${username}`, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json"
+    }
+  })
+    .then(response => {
+      if (response.ok) {
+        return response;
+      }
+      return response.json().then(error => {
+        this.setState({ error });
+        throw new Error(`Request rejected with status ${response.status}`);
+      });
+    })
+    .then(response => response.json());
+};
+
+export { attempt };
--- app/javascript/react/Signup.jsx
+++ app/javascript/react/Signup.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
 import { shape, string } from "prop-types";
+import { attempt } from "../api/Api";
 
 const Signup = ({ user }) => {
   const [username, setUsername] = useState(user.username || "");
+  const [error, setError] = useState();
 
   const updateUsername = event => {
     event.preventDefault();
     setUsername(event.target.value);
+    attempt(event.target.value).then(result => {
+      const { errors } = result;
+      if (errors) {
+        setError(errors);
+      } else {
+        setError();
+      }
+    });
   };
 
   return (
@@ -16,6 +26,7 @@ const Signup = ({ user }) => {
       onChange={updateUsername}
       name="user[username]"
       value={username}
+      className={error ? "field_with_errors" : ""}
     />
   );
 };
