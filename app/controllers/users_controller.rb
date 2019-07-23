d5980b1160214e951fb1ca6e3d88e809b53d64b2
Michael Milewski <saramic@gmail.com>
2019-07-24 06:14:35 +1000

    plain old rails app with a signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(username: params[:username])
 
-    render :new, status: 422 unless @user
+    render :new, status: 404 unless @user
   end
 
   def new
@@ -11,12 +11,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
-    if @user.save
-      redirect_to "/users/#{@user.username}", notice: 'User was successfully created.'
-    else
-      render :new, status: 422
-    end
+    @user.save
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,2 @@
 class User < ApplicationRecord
-  validate :validate_username
-  validates :username, uniqueness: true
-
-  private
-
-  def validate_username
-    if !username.match(/^[A-Za-z0-9_-]+$/)
-      errors.add(:username, "can only have alphanumeric characters")
-    end
-  end
 end
