00d8018a7d1a95bbf767036c8426744b94ccc6f7
Michael Milewski <saramic@gmail.com>
2019-08-01 06:21:26 +1000

    retrospective request spec for users

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET /' do
+    it 'responds with success' do
+      get '/'
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /users/:username' do
+    it 'returns unprocessable for a username not found' do
+      get '/users/non-existant-username'
+      expect(response).to have_http_status(422)
+    end
+
+    context 'my-username exists' do
+      before do
+        User.create(username: 'my-username')
+      end
+
+      it 'returns success for fetching an existing user' do
+        get '/users/my-username'
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  describe 'POST /users' do
+    it 'redirects on successful create' do
+      post '/users', params: { user: { username: 'valid-username' } }
+      expect(response).to have_http_status(302)
+      expect(response.headers.to_hash['Location']).to eq('http://www.example.com/users/valid-username')
+    end
+
+    it 'returns unprocessable with invalid username blank' do
+      post '/users', params: { user: { username: '' } }
+      expect(response).to have_http_status(422)
+    end
+
+    it 'returns unprocessable with invalid username with spaces' do
+      post '/users', params: { user: { username: 'space is not valid' } }
+      expect(response).to have_http_status(422)
+    end
+  end
+end
