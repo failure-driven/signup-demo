c6f52f19ed12e7b8103144268dfcdf6e81553c3b
Michael Milewski <saramic@gmail.com>
2019-08-04 22:05:48 +1000

    twitter does not provide email

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,12 @@ class User < ApplicationRecord
       user.uid = auth.uid
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name   # assuming the user model has a name
-      user.username = auth.info.email[/^[^\@]+\@/].gsub(/[^a-z^A-Z^\-^_^0-9]/, '')
+      if auth.provider == 'twitter'
+        user.username = auth.info.nickname
+      else
+        user.username = auth.info.email[/^[^\@]+\@/].gsub(/[^a-z^A-Z^\-^_^0-9]/, '')
+      end
+
       # user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
