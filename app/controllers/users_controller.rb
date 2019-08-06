6ea382de7b7cad50c3d19d69e7244457301fbe6a
Michael Milewski <saramic@gmail.com>
2019-08-06 22:50:29 +1000

    implementation for controller commented out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.save
+
+    # TODO
+    #
+    # if @user.save
+    #   redirect_to "/users/#{@user.username}",
+    #               notice: 'User was successfully created.'
+    # else
+    #   render :new, status: 422
+    # end
   end
 
   private
