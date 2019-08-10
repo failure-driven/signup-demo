ffd20d4a15d661db07c8cb45e73a1860251e8b6e
Michael Milewski <saramic@gmail.com>
2019-08-10 12:10:19 +1000

    minimize width of React tests for presentation

--- app/javascript/react/InputWithErrors.test.jsx
+++ app/javascript/react/InputWithErrors.test.jsx
@@ -5,9 +5,16 @@ import InputWithErrors from "./InputWithErrors";
 describe("InputWithErrors", () => {
   it("binds username to the input field value", () => {
     const wrapper = shallow(<InputWithErrors data={{}} />);
-    expect(wrapper.find("input").prop("value")).toEqual("");
-    wrapper.find("input").simulate("change", { target: { value: "username" } });
-    expect(wrapper.find("input").prop("value")).toEqual("username");
+    expect(
+      wrapper.find("input").prop("value")
+    ).toEqual("");
+    wrapper.find("input").simulate(
+      "change",
+      { target: { value: "username" } }
+    );
+    expect(
+      wrapper.find("input").prop("value")
+    ).toEqual("username");
   });
 
   // it("pre-populates username from the passed in data", () => {
@@ -16,8 +23,15 @@ describe("InputWithErrors", () => {
   //     modelname: "the_model",
   //     fieldname: "username",
   //   };
-  //   const wrapper = shallow(<InputWithErrors data={data} />);
-  //   expect(wrapper.find("input").prop("value")).toEqual("the username");
-  //   expect(wrapper.find("input").prop("name")).toEqual("the_model[username]");
+  //   const wrapper =
+  //     shallow(<InputWithErrors data={data} />);
+  //
+  //   expect(
+  //     wrapper.find("input").prop("value")
+  //   ).toEqual("the username");
+  //
+  //   expect(
+  //     wrapper.find("input").prop("name")
+  //   ).toEqual("the_model[username]");
   // });
 });
