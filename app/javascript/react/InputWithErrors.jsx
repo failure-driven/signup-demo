01fbaedf6d52c33d947c5cec9a6a0f04d6a91d93
Michael Milewski <saramic@gmail.com>
2023-03-27 10:31:26 +1100

    Actually build out the component

--- app/javascript/react/InputWithErrors.jsx
+++ app/javascript/react/InputWithErrors.jsx
@@ -1,17 +1,50 @@
 import React, { useState } from "react";
 
 const InputWithErrors = () => {
-  const [value, setValue] = useState("");
-
-  const updateState = event => setValue(event.target.value);
-
-  return (
-    <input
-      value={value}
-      name="user[username]"
-      onChange={updateState}
-    />
+  return(
+    null
   );
 };
 
 export default InputWithErrors;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const InputWithErrors = ({ data }) => {
+//   const [value, setValue] = useState(
+//     (data.data && data.data["username"]) || ""
+//   );
+//
+//   const valueChange = event => {
+//     setValue(event.target.value);
+//   };
+//
+//   return (
+//     <input
+//       value={value}
+//       name={`${data.modelname}[${data.fieldname}]`}
+//       onChange={valueChange}
+//     />
+//   );
+// };
+//
+// export default InputWithErrors;
